660e46dc37a1479a239f9568c50675536a36d3b6
Ramiz khan <ramizkhan@MacBook-Pro-4.local>
2018-03-07 17:19:55 -0600

    DB fix?

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 
 //routes - to be replaced by requiring route files
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+    res.render("index");
 });
 
 app.get("/home", function (req, res) {
