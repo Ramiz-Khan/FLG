bd077e45bca503ecad8e7018a76c3f849a447c48
Ramiz khan <ramizkhan@MacBook-Pro-4.local>
2018-03-08 10:55:12 -0600

    DB fix?

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.get("/", function (req, res) {
+app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
